1df27eb081f15efb1d93ba7f67ce3a1860151e5d
Shin <now.nsw@gmail.com>
2022-03-17 23:15:36 +0700

    created useState search func and handleChange func searchbar.js 73/99

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,11 +1,29 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import "./SearchBar.css";
+import { useState } from "react";
 
 function SearchBar(props) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const search = () => {
+    props.onSearch(searchTerm);
+    setSearchTerm("");
+  };
+
+  const handleTermChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <div className="SearchBar">
-      <input placeholder="Enter A Song, Album, or Artist" />
-      <button className="SearchButton">SEARCH</button>
+      <input
+        placeholder="Enter A Song, Album, or Artist"
+        onChange={handleTermChange}
+        value={searchTerm}
+      />
+      <button className="SearchButton" onClick={search}>
+        SEARCH
+      </button>
     </div>
   );
 }
